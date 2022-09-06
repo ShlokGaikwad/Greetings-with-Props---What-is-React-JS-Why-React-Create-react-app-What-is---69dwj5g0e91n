3f00f3b9e9adfc3694a89c91751e85dfc7893c20
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2022-09-06 13:06:53 +0530

    new

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+
+
+
+function Welcome(){
+    const [name, updateData] = useState('shlok')
+    return (
+        <div>
+            <h1>Hey!{name}</h1>
+            <h2>Welcome to Newton School.</h2>
+        </div>
+        
+    )
+}
+export default Welcome;
